00b88282172fa453563d51ede224ccc770b26afb
huanchaunhat <huanchaunhat@gmail.com>
2024-10-30 10:14:36 +0700

    Nop Bai Kiem Tra Web Themeleaf

---
+++ src/main/java/vn/iostar/themeleaf/ThemeleafApplication.java
@@ -0,0 +1,13 @@
+package vn.iostar.themeleaf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThemeleafApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThemeleafApplication.class, args);
+	}
+
+}
---
+++ src/test/java/vn/iostar/themeleaf/ThemeleafApplicationTests.java
@@ -0,0 +1,13 @@
+package vn.iostar.themeleaf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThemeleafApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
